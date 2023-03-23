ee73ed227f850a188f4983fb41e52836b0191de7
a-deep97 <a.deep97@outlook.com>
2023-03-23 23:31:01 +0530

    Added some ui on side panel inclusing quick links

--- task_manager_ui/src/static/js/components/side-panel.js
+++ task_manager_ui/src/static/js/components/side-panel.js
@@ -9,7 +9,15 @@ class SidePanel extends Component{
     render(){
         return(
             <div className="side-panel">
-                <p>This is side panel</p>
+                <div id="side-panel-heading">Tasks</div>
+                <button id="create-task-btn"> Create Task</button>
+                <div id="side-panel-contents">
+                    <div id="side-panel-link-container">
+                        <div id="my-tasks" className="side-panel-links">My Tasks</div>
+                        <div id="my-projects" className="side-panel-links">My Projects</div>
+                        <div id="my-tasks" className="side-panel-links">My Tasks</div>
+                    </div>
+                </div>
             </div>
         );
     };
