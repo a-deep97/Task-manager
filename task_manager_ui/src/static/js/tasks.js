7b37410f9a8541dddf646d02aca8c86fb32a80fc
a-deep97 <a.deep97@outlook.com>
2023-03-23 16:12:00 +0530

    Added basic components into user , tasks page as well

--- task_manager_ui/src/static/js/tasks.js
+++ task_manager_ui/src/static/js/tasks.js
@@ -1,13 +1,20 @@
+import '../css/common.css';
 import { Component } from "react";
 import Navbar from "./components/navbar";
-import '../css/common.css';
+import MainContainer from './components/main-container';
+import Footer from './components/footer';
+
+/**
+ * This component represents tasks page
+ */
 
 class Tasks extends Component{
     render(){
         return(
-            <div className="tasks">
+            <div id="tasks" className="main-view">
                 <Navbar />
-                <h1>This is tasks page</h1>
+                <MainContainer />
+                <Footer />
             </div>
         );
     }
--- task_manager_ui/src/static/js/user.js
+++ task_manager_ui/src/static/js/user.js
@@ -1,13 +1,20 @@
+import '../css/common.css';
 import { Component } from "react";
 import Navbar from "./components/navbar";
-import '../css/common.css';
+import MainContainer from './components/main-container';
+import Footer from './components/footer';
+
+/**
+ * THis component represents user page
+ */
 
 class User extends Component{
     render(){
         return(
-            <div className="user">
+            <div id="user" className="main-view">
                 <Navbar />
-                <h1>This is user page</h1>
+                <MainContainer />
+                <Footer />
             </div>
         );
     }
