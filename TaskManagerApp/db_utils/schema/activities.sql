e8eeda21b7d962fe83b07e76324326daaa214091
a-deep97 <a.deep97@outlook.com>
2023-11-22 11:20:16 +0530

    Added schema for task activities

---
+++ TaskManagerApp/db_utils/schema/activities.sql
@@ -0,0 +1,12 @@
+CREATE TABLE TaskActivities (
+  id INT AUTO_INCREMENT,
+  task_id INT NOT NULL,
+  comment VARCHAR(255) DEFAULT NULL,
+  activity VARCHAR(50) DEFAULT NULL,
+  activity_order INT NOT NULL,
+  deleted BOOLEAN DEFAULT FALSE,
+  activity_date DATE,
+  activity_time TIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (task_id) REFERENCES tasks(id)
+);
