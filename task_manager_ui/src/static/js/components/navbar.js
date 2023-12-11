9dc24b9d8c3d36134ba4dd276588ed5f96ff76f2
a-deep97 <a.deep97@outlook.com>
2023-12-11 15:36:59 +0530

    minor changes in navbar component

--- task_manager_ui/src/static/js/components/navbar.js
+++ task_manager_ui/src/static/js/components/navbar.js
@@ -1,18 +1,15 @@
-import { Component } from "react";
-import '../../css/navbar.css';
-import LogOut from "./logout";
+import React from 'react';
+import LogOut from './logout';
 
-class Navbar extends Component{
-    render(){
-        return(
-            <div className="navbar">
-                <div id="navbar-title">Task Manager</div>
-                <div className="navbar-links">
-                    <LogOut/>
-                </div>      
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div id="navbar-title">Task Manager</div>
+            <div className="navbar-links">
+                <LogOut />
             </div>
-        );
-    }
-};
+        </div>
+    );
+}
 
-export default Navbar
+export default Navbar;
